c7ffdf406dea9460795a1e7944009f4958b8c6a5
“lichtle” <leticia.negreiros@outlook.com>
2023-12-05 21:44:36 -0300

    chore: exercício quase finalizado (erro no ::before e no :hover)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import EstiloGlobal, { Container } from './global'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import './global.ts'
 
 function App() {
   return (
     <>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/Cabecalho/styles.ts
+++ src/components/Cabecalho/styles.ts
@@ -1,7 +1,9 @@
-.cabecalho {
+import styled from 'styled-components'
+import '../../variables.css'
+
+export const Header = styled.header`
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
   text-align: center;
   padding: 24px 0;
-}
-
+`
